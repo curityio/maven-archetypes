9feb944ec953b1fe177c3b59fda5791ea5570c9a
Atif Saddique <atif.saddique4@gmail.com>
2017-12-15 18:36:18 +0500

    add readme, license and gitignore

---
+++ src/main/resources/archetype-resources/deploy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cp  ./\$\{DIST_DIR\}/lib/plugins/authenticator-custom/${artifactId}-${version}.jar ~/Documents/programs/curity/idsvr-2.2.2/idsvr/lib/plugins/authenticator/${artifactId}-${version}.jar
+
