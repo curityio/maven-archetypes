880cecd1550676add168ad48317fa12ef14a4e13
Atif Saddique <atif.saddique4@gmail.com>
2018-01-17 21:19:27 +0500

    update java oauth maven archetype

--- oauth-authenticator-archetype/src/main/resources/archetype-resources/src/main/java/authentication/CallbackRequestHandler.java
+++ oauth-authenticator-archetype/src/main/resources/archetype-resources/src/main/java/authentication/CallbackRequestHandler.java
@@ -1,65 +1,213 @@
 package ${package}.authentication;
 
 import ${package}.config.${pluginName}AuthenticatorPluginConfig;
-import io.curity.identityserver.plugin.authentication.DefaultOAuthClient;
-import io.curity.identityserver.plugin.authentication.OAuthClient;
-import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import se.curity.identityserver.sdk.Nullable;
+import se.curity.identityserver.sdk.attribute.Attribute;
+import se.curity.identityserver.sdk.attribute.Attributes;
+import se.curity.identityserver.sdk.attribute.AuthenticationAttributes;
+import se.curity.identityserver.sdk.attribute.ContextAttributes;
+import se.curity.identityserver.sdk.attribute.SubjectAttributes;
 import se.curity.identityserver.sdk.authentication.AuthenticationResult;
 import se.curity.identityserver.sdk.authentication.AuthenticatorRequestHandler;
+import se.curity.identityserver.sdk.errors.ErrorCode;
+import se.curity.identityserver.sdk.http.HttpRequest;
+import se.curity.identityserver.sdk.http.HttpResponse;
 import se.curity.identityserver.sdk.service.ExceptionFactory;
+import se.curity.identityserver.sdk.service.HttpClient;
 import se.curity.identityserver.sdk.service.Json;
+import se.curity.identityserver.sdk.service.WebServiceClient;
+import se.curity.identityserver.sdk.service.WebServiceClientFactory;
 import se.curity.identityserver.sdk.service.authentication.AuthenticatorInformationProvider;
 import se.curity.identityserver.sdk.web.Request;
 import se.curity.identityserver.sdk.web.Response;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
-import static io.curity.identityserver.plugin.authentication.Constants.Params.PARAM_ACCESS_TOKEN;
-
-public class CallbackRequestHandler
-        implements AuthenticatorRequestHandler<CallbackRequestModel> {
+public class CallbackRequestHandler implements AuthenticatorRequestHandler<CallbackRequestModel>
+{
+    private final static Logger _logger = LoggerFactory.getLogger(CallbackRequestHandler.class);
 
     private final ExceptionFactory _exceptionFactory;
-    private final OAuthClient _oauthClient;
     private final ${pluginName}AuthenticatorPluginConfig _config;
+    private final Json _json;
+    private final AuthenticatorInformationProvider _authenticatorInformationProvider;
+    private final WebServiceClientFactory _webServiceClientFactory;
 
-    public CallbackRequestHandler(ExceptionFactory exceptionFactory,
-                                  AuthenticatorInformationProvider provider,
-                                  Json json,
-                                  ${pluginName}AuthenticatorPluginConfig config) {
-        _exceptionFactory = exceptionFactory;
-        _oauthClient = new DefaultOAuthClient(exceptionFactory, provider, json, config.getSessionManager());
+    public CallbackRequestHandler(${pluginName}AuthenticatorPluginConfig config)
+    {
+        _exceptionFactory = config.getExceptionFactory();
         _config = config;
+        _json = config.getJson();
+        _webServiceClientFactory = config.getWebServiceClientFactory();
+        _authenticatorInformationProvider = config.getAuthenticatorInformationProvider();
     }
 
     @Override
-    public CallbackRequestModel preProcess(Request request, Response response) {
-        if (request.isGetRequest()) {
+    public CallbackRequestModel preProcess(Request request, Response response)
+    {
+        if (request.isGetRequest())
+        {
             return new CallbackRequestModel(request);
-        } else {
+        } else
+        {
             throw _exceptionFactory.methodNotAllowed();
         }
     }
 
     @Override
-    public Optional<AuthenticationResult> get(CallbackRequestModel requestModel,
-                                              Response response) {
-        _oauthClient.redirectToAuthenticationOnError(requestModel.getRequest(), _config.id());
-
-        Map<String, Object> tokenMap = _oauthClient.getTokens(_config.getTokenEndpoint().toString(),
-                _config.getClientId(),
-                _config.getClientSecret(),
-                requestModel.getCode(),
-                requestModel.getState());
-
-        return _oauthClient.getAuthenticationResult(
-            tokenMap.get(PARAM_ACCESS_TOKEN).toString(), _config.getUserInfoEndpoint().toString());
+    public Optional<AuthenticationResult> post(CallbackRequestModel requestModel, Response response)
+    {
+        throw _exceptionFactory.methodNotAllowed();
     }
 
     @Override
-    public Optional<AuthenticationResult> post(CallbackRequestModel requestModel, Response response) {
-        throw _exceptionFactory.methodNotAllowed();
+    public Optional<AuthenticationResult> get(CallbackRequestModel requestModel, Response response)
+    {
+        validateState(requestModel.getState());
+        handleError(requestModel);
+
+        Map<String, Object> tokenResponseData = redeemCodeForTokens(requestModel);
+
+        List<Attribute> subjectAttributers = new ArrayList<>();
+        subjectAttributers.add(Attribute.of("id", Objects.toString(tokenResponseData.get("id"))));
+        
+
+        AuthenticationAttributes attributes = AuthenticationAttributes.of(
+                SubjectAttributes.of(tokenResponseData.get("id").toString(), Attributes.of(subjectAttributers)),
+                ContextAttributes.of(Attributes.of(Attribute.of("access_token", tokenResponseData.get("access_token").toString()))));
+        AuthenticationResult authenticationResult = new AuthenticationResult(attributes);
+        return Optional.ofNullable(authenticationResult);
+    }
+
+    private Map<String, Object> redeemCodeForTokens(CallbackRequestModel requestModel)
+    {
+        HttpResponse tokenResponse = getWebServiceClient()
+                .withPath("/oauth/v2/accessToken")
+                .request()
+                .contentType("application/x-www-form-urlencoded")
+                .body(getFormEncodedBodyFrom(createPostData(_config.getClientId(), _config.getClientSecret(),
+                        requestModel.getCode(), requestModel.getRequestUrl())))
+                .method("POST")
+                .response();
+        int statusCode = tokenResponse.statusCode();
+
+        if (statusCode != 200)
+        {
+            if (_logger.isInfoEnabled())
+            {
+                _logger.info("Got error response from token endpoint: error = {}, {}", statusCode,
+                        tokenResponse.body(HttpResponse.asString()));
+            }
+
+            throw _exceptionFactory.internalServerException(ErrorCode.EXTERNAL_SERVICE_ERROR);
+        }
+
+        return _json.fromJson(tokenResponse.body(HttpResponse.asString()));
+    }
+
+    private WebServiceClient getWebServiceClient()
+    {
+        Optional<HttpClient> httpClient = _config.getHttpClient();
+
+        if (httpClient.isPresent())
+        {
+            return _webServiceClientFactory.create(httpClient.get()).withHost("host.server.com");
+        } else
+        {
+            return _webServiceClientFactory.create(URI.create("https://host.server.com"));
+        }
+    }
+
+    private void handleError(CallbackRequestModel requestModel)
+    {
+        if (!Objects.isNull(requestModel.getError()))
+        {
+            if ("access_denied".equals(requestModel.getError()))
+            {
+                _logger.debug("Got an error from ${pluginName}: {} - {}", requestModel.getError(), requestModel.getErrorDescription());
+
+                throw _exceptionFactory.redirectException(
+                        _authenticatorInformationProvider.getAuthenticationBaseUri().toASCIIString());
+            }
+
+            _logger.warn("Got an error from ${pluginName}: {} - {}", requestModel.getError(), requestModel.getErrorDescription());
+
+            throw _exceptionFactory.externalServiceException("Login with ${pluginName} failed");
+        }
+    }
+
+    private static Map<String, String> createPostData(String clientId, String clientSecret, String code, String callbackUri)
+    {
+        Map<String, String> data = new HashMap<>(5);
+
+        data.put("client_id", clientId);
+        data.put("client_secret", clientSecret);
+        data.put("code", code);
+        data.put("grant_type", "authorization_code");
+        data.put("redirect_uri", callbackUri);
+
+        return data;
+    }
+
+    private static HttpRequest.BodyProcessor getFormEncodedBodyFrom(Map<String, String> data)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        data.entrySet().forEach(e -> appendParameter(stringBuilder, e));
+
+        return HttpRequest.fromString(stringBuilder.toString());
+    }
+
+    private static void appendParameter(StringBuilder stringBuilder, Map.Entry<String, String> entry)
+    {
+        String key = entry.getKey();
+        String value = entry.getValue();
+        String encodedKey = urlEncodeString(key);
+        stringBuilder.append(encodedKey);
+
+        if (!Objects.isNull(value))
+        {
+            String encodedValue = urlEncodeString(value);
+            stringBuilder.append("=").append(encodedValue);
+        }
+
+        stringBuilder.append("&");
+    }
+
+    private static String urlEncodeString(String unencodedString)
+    {
+        try
+        {
+            return URLEncoder.encode(unencodedString, StandardCharsets.UTF_8.name());
+        } catch (UnsupportedEncodingException e)
+        {
+            throw new RuntimeException("This server cannot support UTF-8!", e);
+        }
+    }
+
+    private void validateState(String state)
+    {
+        @Nullable Attribute sessionAttribute = _config.getSessionManager().get("state");
+
+        if (sessionAttribute != null && state.equals(sessionAttribute.getValueOfType(String.class)))
+        {
+            _logger.debug("State matches session");
+        } else
+        {
+            _logger.debug("State did not match session");
+
+            throw _exceptionFactory.badRequestException(ErrorCode.INVALID_SERVER_STATE, "Bad state provided");
+        }
     }
 }
--- oauth-authenticator-archetype/src/main/resources/archetype-resources/src/main/java/authentication/__pluginName__AuthenticatorRequestHandler.java
+++ oauth-authenticator-archetype/src/main/resources/archetype-resources/src/main/java/authentication/__pluginName__AuthenticatorRequestHandler.java
@@ -1,57 +1,97 @@
 package ${package}.authentication;
 
 import ${package}.config.${pluginName}AuthenticatorPluginConfig;
-import com.google.common.collect.ImmutableMap;
-import io.curity.identityserver.plugin.authentication.DefaultOAuthClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import se.curity.identityserver.sdk.attribute.Attribute;
 import se.curity.identityserver.sdk.authentication.AuthenticationResult;
 import se.curity.identityserver.sdk.authentication.AuthenticatorRequestHandler;
+import se.curity.identityserver.sdk.errors.ErrorCode;
+import se.curity.identityserver.sdk.http.RedirectStatusCode;
 import se.curity.identityserver.sdk.service.ExceptionFactory;
-import se.curity.identityserver.sdk.service.Json;
 import se.curity.identityserver.sdk.service.authentication.AuthenticatorInformationProvider;
 import se.curity.identityserver.sdk.web.Request;
 import se.curity.identityserver.sdk.web.Response;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
 
-import static io.curity.identityserver.plugin.authentication.Constants.Params.PARAM_REDIRECT_URI;
+import static ${package}.descriptor.${pluginName}AuthenticatorPluginDescriptor.CALLBACK;
 
-public class ${pluginName}AuthenticatorRequestHandler implements AuthenticatorRequestHandler<RequestModel> {
+public class ${pluginName}AuthenticatorRequestHandler implements AuthenticatorRequestHandler<Request>
+{
     private static final Logger _logger = LoggerFactory.getLogger(${pluginName}AuthenticatorRequestHandler.class);
+    private static final String AUTHORIZATION_ENDPOINT = "";
 
     private final ${pluginName}AuthenticatorPluginConfig _config;
-    private final DefaultOAuthClient _oauthClient;
+    private final AuthenticatorInformationProvider _authenticatorInformationProvider;
+    private final ExceptionFactory _exceptionFactory;
 
-    public ${pluginName}AuthenticatorRequestHandler(${pluginName}AuthenticatorPluginConfig config,
-                                                    ExceptionFactory exceptionFactory,
-                                                    Json json,
-                                                    AuthenticatorInformationProvider provider) {
+    public ${pluginName}AuthenticatorRequestHandler(${pluginName}AuthenticatorPluginConfig config)
+    {
         _config = config;
-        _oauthClient = new DefaultOAuthClient(exceptionFactory, provider, json, config.getSessionManager());
+        _exceptionFactory = config.getExceptionFactory();
+        _authenticatorInformationProvider = config.getAuthenticatorInformationProvider();
     }
 
     @Override
-    public Optional<AuthenticationResult> get(RequestModel requestModel, Response response) {
-        _logger.info("GET request received for authentication");
+    public Optional<AuthenticationResult> get(Request request, Response response)
+    {
+        _logger.debug("GET request received for authentication");
 
-        _oauthClient.setServiceProviderId(requestModel.getRequest());
+        String redirectUri = createRedirectUri();
+        String state = UUID.randomUUID().toString();
+        Map<String, Collection<String>> queryStringArguments = new LinkedHashMap<>(5);
+        Set<String> scopes = new LinkedHashSet<>(7);
 
-        _oauthClient.redirectToAuthorizationEndpoint(response,
-                _config.getAuthorizationEndpoint().toString(),
-                _config.getClientId(),
-                _config.getScope(), ImmutableMap.of(PARAM_REDIRECT_URI, _oauthClient.getCallbackUrl()));
+        _config.getSessionManager().put(Attribute.of("state", state));
 
-        return Optional.empty();
+        queryStringArguments.put("client_id", Collections.singleton(_config.getClientId()));
+        queryStringArguments.put("redirect_uri", Collections.singleton(redirectUri));
+        queryStringArguments.put("state", Collections.singleton(state));
+        queryStringArguments.put("response_type", Collections.singleton("code"));
+
+        queryStringArguments.put("scope", Collections.singleton(String.join(" ", scopes)));
+
+        _logger.debug("Redirecting to {} with query string arguments {}", AUTHORIZATION_ENDPOINT,
+                queryStringArguments);
+
+        throw _exceptionFactory.redirectException(AUTHORIZATION_ENDPOINT,
+                RedirectStatusCode.MOVED_TEMPORARILY, queryStringArguments, false);
+    }
+
+    private String createRedirectUri()
+    {
+        try
+        {
+            URI authUri = _authenticatorInformationProvider.getFullyQualifiedAuthenticationUri();
+
+            return new URL(authUri.toURL(), authUri.getPath() + "/" + CALLBACK).toString();
+        } catch (MalformedURLException e)
+        {
+            throw _exceptionFactory.internalServerException(ErrorCode.INVALID_REDIRECT_URI,
+                    "Could not create redirect URI");
+        }
     }
 
     @Override
-    public Optional<AuthenticationResult> post(RequestModel requestModel, Response response) {
-        return Optional.empty();
+    public Optional<AuthenticationResult> post(Request request, Response response)
+    {
+        throw _exceptionFactory.methodNotAllowed();
     }
 
     @Override
-    public RequestModel preProcess(Request request, Response response) {
-        return new RequestModel(request);
+    public Request preProcess(Request request, Response response)
+    {
+        return request;
     }
 }
