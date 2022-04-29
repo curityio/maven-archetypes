ed8d5ef34c631ab0982d0f6f9a2bfdfc2b1da689
Michal Trojanowski <michal@curity.io>
2022-04-29 16:58:44 +0200

    Review updates

--- kotlin-oauth-authenticator-archetype/src/main/resources/archetype-resources/src/main/kotlin/authentication/RedirectUriUtil.kt
+++ kotlin-oauth-authenticator-archetype/src/main/resources/archetype-resources/src/main/kotlin/authentication/RedirectUriUtil.kt
@@ -1,27 +1,27 @@
 package ${package}.authentication
 
-import se.curity.identityserver.sdk.errors.ErrorCode
+import se.curity.identityserver.sdk.errors.ErrorCode.INVALID_REDIRECT_URI
 import se.curity.identityserver.sdk.service.ExceptionFactory
 import se.curity.identityserver.sdk.service.authentication.AuthenticatorInformationProvider
 
 import java.net.MalformedURLException
-import java.net.URI
 import java.net.URL
 
 import ${package}.descriptor.${pluginName}AuthenticatorPluginDescriptor.Companion.CALLBACK
 
-final class RedirectUriUtil()
+class RedirectUriUtil
 {
+    private constructor()
 
     companion object {
-        public fun createRedirectUri(authenticatorInformationProvider: AuthenticatorInformationProvider,
+        fun createRedirectUri(authenticatorInformationProvider: AuthenticatorInformationProvider,
         exceptionFactory: ExceptionFactory): String
         {
             try
             {
-                val authUri = authenticatorInformationProvider.getFullyQualifiedAuthenticationUri()
+                val authUri = authenticatorInformationProvider.fullyQualifiedAuthenticationUri
 
-                return URL(authUri.toURL(), authUri.getPath() + "/" + CALLBACK).toString()
+                return URL(authUri.toURL(), authUri.path + "/" + CALLBACK).toString()
             }
             catch (e: MalformedURLException)
             {
