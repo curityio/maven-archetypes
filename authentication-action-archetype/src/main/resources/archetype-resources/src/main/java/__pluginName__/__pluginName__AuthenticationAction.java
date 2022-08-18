1165d3370ba96c6fee5e29e5bf6ac715281fdc59
Judith Kahrer <judith@curity.io>
2022-08-18 13:40:38 +0200

    Update authentication actions to use new interface

--- authentication-action-archetype/src/main/resources/archetype-resources/src/main/java/__pluginName__/__pluginName__AuthenticationAction.java
+++ authentication-action-archetype/src/main/resources/archetype-resources/src/main/java/__pluginName__/__pluginName__AuthenticationAction.java
@@ -16,16 +16,13 @@ public final class ${pluginName}AuthenticationAction implements AuthenticationAc
     }
 
     @Override
-    public AuthenticationActionResult apply(AuthenticationAttributes authenticationAttributes,
-                                            AuthenticatedSessions authenticatedSessions,
-                                            String authenticationTransactionId,
-                                            AuthenticatorDescriptor authenticatorDescriptor)
+    public AuthenticationActionResult apply(AuthenticationActionContext context)
     {
 
         // Have a look at this example: https://github.com/curityio/time-authentication-action
         // to see what you can do in a plugin. If you prefer going straight to the docs, here's a link:
-        // https://developer.curity.io/docs/latest/developer-guide/plugins/index.html
+        // https://curity.io/docs/idsvr/latest/developer-guide/plugins/index.html
 
-        return AuthenticationActionResult.successfulResult(authenticationAttributes);
+        return AuthenticationActionResult.successfulResult(context.getAuthenticationAttributes());
     }
 }
--- kotlin-authentication-action-archetype/src/main/resources/archetype-resources/src/main/kotlin/__pluginName__/__pluginName__AuthenticationAction.kt
+++ kotlin-authentication-action-archetype/src/main/resources/archetype-resources/src/main/kotlin/__pluginName__/__pluginName__AuthenticationAction.kt
@@ -8,16 +8,13 @@ import se.curity.identityserver.sdk.service.authenticationaction.AuthenticatorDe
 
 class ${pluginName}AuthenticationAction(private val _configuration: ${pluginName}AuthenticationActionConfig): AuthenticationAction
 {
-    override fun apply(authenticationAttributes: AuthenticationAttributes,
-                       authenticatedSessions: AuthenticatedSessions,
-                       authenticationTransactionId: String,
-                       authenticatorDescriptor: AuthenticatorDescriptor): AuthenticationActionResult
+    override fun apply(context: AuthenticationActionContext): AuthenticationActionResult
     {
 
         // Have a look at this example: https://github.com/curityio/time-authentication-action
         // to see what you can do in a plugin. If you prefer going straight to the docs, here's a link:
-        // https://developer.curity.io/docs/latest/developer-guide/plugins/index.html
+        // https://curity.io/docs/idsvr/latest/developer-guide/plugins/index.html
 
-        return AuthenticationActionResult.successfulResult(authenticationAttributes)
+        return AuthenticationActionResult.successfulResult(context.getAuthenticationAttributes())
     }
 }
